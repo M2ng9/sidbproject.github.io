571fe9044b8561eda7b7439cb316da4af1e286e0
avvan2 <adee08setiawan@gmail.com>
2023-05-11 09:49:27 +0700

    terbaru v14

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         secondary: '#334155',
         third: '#f1f5f9',
         dark: '#1c1917',
+        grays: '#cbd5e1',
       },
       screens: {
         '2xl': '1320px', 
