a999e47218e4e9fea6a39b93926fdbdf5bb5c0ff
ade <mamankgamin9@gmail.com>
2023-06-13 16:46:37 +0700

    perubahan kembali ke awal

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
-$conn= new mysqli('localhost','root','','sidesta_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli('localhost', 'root', '', 'sidesta_db') or die("Could not connect to mysql" . mysqli_error($conn));
 ?>
